6b796b1213dfc610a4f0c324dcf4dc2ef843f02d
Nemantaj Garg <nemantaj@gmail.com>
2022-12-10 00:36:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://app-pet-api-6jjd.vercel.app/:path*'
+        destination: 'http://app-pet-api-6jjd.vercel.app/:path*'
       }
     ]
   }
